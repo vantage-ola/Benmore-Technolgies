97109f3354f8d020d762f0e37e8aab5d491d0184
Dozie2001 <davidchidozie402@gmail.com>
2024-06-17 22:54:30 +0100

    Setup templates and tailwindCss deleted some files

---
+++ Tasky/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./templates/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'faint-purple': '#eeebfe',
+        'font-purple': '#afacf0',
+        'faint-black': '#2f3438',
+        'faint-gray': '#f6f8ff',
+        'blue-progress': '#9abaf3',
+        'green-progress': '#24d366',
+        'meeting-green': '#e9fbef',
+        'circle-orange': '#f6a49b',
+      },
+    },
+  },
+  plugins: [],
+}
+
